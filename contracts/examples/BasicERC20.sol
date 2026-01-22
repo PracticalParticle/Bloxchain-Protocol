5c9ea549b0c77bdd6fd3ff5f683bc39a316c9d79
JaCoderX <jaco1234h@gmail.com>
2026-01-23 00:30:18 +0200

    change example contracts License to MIT

--- contracts/examples/BasicERC20.sol
+++ contracts/examples/BasicERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
--- contracts/examples/SimpleRWA20/SimpleRWA20.sol
+++ contracts/examples/SimpleRWA20/SimpleRWA20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MPL-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 // OpenZeppelin imports
